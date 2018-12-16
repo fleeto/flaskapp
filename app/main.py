85ee4a79b378a3265bf0a8007c5fc67b1b630569
Vincent Cui <github@fleeto.us>
2018-12-17 01:35:09 +0800

    added: new function

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 from flask import Flask, request
 import os
 import urllib.request
+import json
 
 app = Flask(__name__)
 
@@ -10,10 +11,17 @@ def show_env(env):
     return os.environ.get(env)
 
 @app.route('/fetch')
-def fetch_env():
+def fetch_url():
     url = request.args.get('url', '')
     with urllib.request.urlopen(url) as response:
         return response.read()
+
+@app.route('/fetch_with_header')
+def fetch_with_headers():
+    url = request.args.get('url', '')
+    request_headers = request.headers
+    with urllib.request.urlopen(url) as response:
+        return  "{}\n{}".format(json.dumps(request_headers), response.read())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
