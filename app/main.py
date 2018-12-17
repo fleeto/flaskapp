fbae6abbac2656f4904c6c2c018058f0e41fb93b
Vincent Cui <github@fleeto.us>
2018-12-17 10:56:39 +0800

    added: tracing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 from flask import Flask, request
 import os
-import urllib.request
+from urllib.request import Request, urlopen
 import json
 
 app = Flask(__name__)
+
+TRACE_HEADERS = [
+    'x-request-id',
+    'x-b3-traceid',
+    'x-b3-spanid',
+    'x-b3-parentspanid',
+    'x-b3-sampled',
+    'x-b3-flags',
+    'x-ot-span-context'
+]
 
 @app.route('/env/<env>')
 def show_env(env):
@@ -13,15 +23,28 @@ def show_env(env):
 @app.route('/fetch')
 def fetch_url():
     url = request.args.get('url', '')
-    with urllib.request.urlopen(url) as response:
+    with urlopen(url) as response:
         return response.read()
 
 @app.route('/fetch_with_header')
 def fetch_with_headers():
     url = request.args.get('url', '')
     request_headers = dict(request.headers)
-    with urllib.request.urlopen(url) as response:
+    with urlopen(url) as response:
         return  "{}\n{}".format(json.dumps(request_headers), response.read())
+
+@app.route('/fetch_with_trace')
+def fetch_with_trace():
+    url = request.args.get('url', '')
+    request_headers = dict(request.headers)
+    new_header = {}
+    for key in request_headers.keys():
+        if key in TRACE_HEADERS:
+            new_header[key] = request_headers[key]
+
+    req = Request(url, headers = new_header)
+    return urlopen(req).read()
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
